4eebba8fd9c5cdaca2025937ae042255cd6ce5d7
Ben Ellis <benjaminster@gmail.com>
2012-10-20 02:22:03 -0400

    Add expiration and query filtering to event queue

--- src/ble/game/agent_test.go
+++ src/ble/game/agent_test.go
@@ -6,6 +6,8 @@ import (
 	. "time"
 )
 
+var time0 Time
+
 func Test_Shutdown_Via_Close(t *T) {
 	agent := NewGame()
 	reachAround := agent.Game.Events
@@ -65,7 +67,7 @@ func Test_Getting_Game_Events(t *T) {
 	agent.PassSequence(a1.Id)
 	agent.PassSequence(a0.Id)
 	_ = <-After(100 * Millisecond)
-	var time0 Time
+	//	var time0 Time
 	t.Log(time0)
 	events, time1 := agent.GetGameEvents(time0)
 	t.Log(time1)
@@ -73,4 +75,46 @@ func Test_Getting_Game_Events(t *T) {
 		bytes, _ := json.Marshal(v)
 		t.Log(string(bytes))
 	}
+	agent.Shutdown()
+}
+
+func Test_Event_Expiration(t *T) {
+	agent := newGameAgent(325*Millisecond, 25*Millisecond)
+	_, _ = agent.AddArtist("sammy")
+	_ = <-After(80 * Millisecond)
+	_, _ = agent.AddArtist("buffalo vincent")
+	_ = <-After(80 * Millisecond)
+	agent.Start()
+
+	var time0 Time
+	es1, _ := agent.GetGameEvents(time0)
+	_ = <-After(200 * Millisecond)
+	es2, _ := agent.GetGameEvents(time0)
+	_ = <-After(100 * Millisecond)
+	es3, _ := agent.GetGameEvents(time0)
+	t.Log(es1)
+	t.Log(es2)
+	t.Log(es3)
+	if len(es1) < len(es2) || len(es2) < len(es3) {
+		t.Error("race-conditioned our way to nonsense land")
+	}
+	agent.Shutdown()
+}
+
+func Test_Event_Filtration(t *T) {
+
+	agent := NewGame()
+	agent.AddArtist("without spaces")
+	_, time1 := agent.GetGameEvents(time0)
+	es2, _ := agent.GetGameEvents(time1)
+	agent.Start()
+	es3, _ := agent.GetGameEvents(time1)
+	if len(es2) != 0 {
+		t.Error("failed to filter out event prior to query time")
+	}
+	if len(es3) != 1 {
+		t.Error("failed to return single relevant event")
+	}
+
+	agent.Shutdown()
 }
