ea81415cbab86d5ac07765203f64ddacc31cfddb
Ben Ellis <benjaminster@gmail.com>
2013-01-02 20:42:25 -0500

    Starting work on making that join client work

--- src/main/main.go
+++ src/main/main.go
@@ -1,13 +1,47 @@
 package main
 
 import (
-	"ble/game"
-	"ble/game/handler"
-	"log"
+	"ble/tpg/persistence"
+	"ble/tpg/switchboard"
+	"fmt"
+	"net"
 	. "net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	handlerGame := handler.NewRoomHandler(game.NewGame())
-	log.Fatal(ListenAndServe(":24769", handlerGame))
+	tmpDbFile := "db"
+	port := ":24769"
+	hostname := "localhost"
+	base := "http://" + hostname + port
+
+	fileAfter := fmt.Sprintf("%s-%s", tmpDbFile, time.Now())
+	backend, err := persistence.NewBackend(tmpDbFile)
+	defer os.Rename(tmpDbFile, fileAfter)
+	sb := switchboard.NewSwitchboard(backend)
+
+	game, _ := backend.CreateGame("thangdangdoodle")
+	gameUrlBase := sb.URLOf(game)
+	gameJoinUrl, _ := url.Parse(base + gameUrlBase.Path + "join-client")
+
+	ephemera := sb.GetEphemera()
+	ephemeralJoin := ephemera.NewCreateUser("benjaminstarrr", "the.bomb@thebomb.com", "asdfqweruxl", gameJoinUrl)
+	ephUrl, _ := url.Parse(base + sb.URLOf(ephemeralJoin).Path)
+
+	server := Server{
+		Addr:         port,
+		Handler:      sb,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second}
+	listener, _ := net.Listen("tcp", port)
+	go server.Serve(listener)
+	chInterrupt := make(chan os.Signal)
+	signal.Notify(chInterrupt, os.Interrupt)
+	select {
+	case <-chInterrupt:
+		return
+	}
 }
