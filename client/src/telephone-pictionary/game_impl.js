3394768712de316005832a667336862fbcb0158a
Ben Ellis <benjaminster@gmail.com>
2013-04-29 20:04:04 -0400

    moving slowly along

--- client/src/telephone-pictionary/game_impl.js
+++ client/src/telephone-pictionary/game_impl.js
@@ -1,5 +1,5 @@
 
-goog.provide('ble.telephone_pictionary.game_impl.Game');
+goog.provide('ble.telephone_pictionary.GameImpl');
 
 goog.require('ble.telephone_pictionary.Game');
 
@@ -11,105 +11,112 @@ goog.require('ble.scribbleDeserializer');
 
 goog.scope(function() {
 var _ = ble.telephone_pictionary;
-var __ = ble.telephone_pictionary.game_impl;
 var Result = goog.result.Result;
 var result = goog.result;
 var EventTarget = goog.events.EventTarget;
-/**
- * @constructor
- */ 
-__.GameState = function() {
-  this.id = "";
-  this.isComplete = false;
-  this.isStarted = false;
-  this.lastTime = 0;
-  this.players = [];
-  this.stacks = [];
-  this.stacksInPlay = [];
-  this.url = "";
-  this.playerMe = null;
-};
 
 /**
  * @constructor
  * @extends {EventTarget}
  * @param {_.Client} client
- * @param {__.State=} state
+ * @param {_.GameState=} state
  * @implements {_.Game}
  */
-__.Game = function(client, state) {
+_.GameImpl= function(client, state) {
   this.client = client; 
-  this.state = goog.isDefAndNotNull(state) ? state : new __.GameState();
+  this.state = goog.isDefAndNotNull(state) ? state : new _.GameState();
 };
-goog.inherits(_.game_impl.Game, EventTarget);
+goog.inherits(_.GameImpl, EventTarget);
 
-__.Game.prototype.fetchState = function() {
+_.GameImpl.prototype.fetchState = function() {
   var requested = this.client.getGameState();
   requested.wait(goog.bind(this.finishFetchState, this));
   return result.transform(requested, function(result) { return result; });
 };
 
-__.Game.prototype.isStarted = function(){ return this.started; };
+_.GameImpl.prototype.isStarted = function(){ return this.state.isStarted; };
 
-__.Game.prototype.isFinished = function(){ return this.finished; };
+_.GameImpl.prototype.isFinished = function(){ return this.state.isComplete; };
 
-__.Game.prototype.players = function(){ return this.aPlayers.slice(); };
+_.GameImpl.prototype.players = function(){ return this.state.players.slice(); };
 
-__.Game.prototype.playersById = function(){
-  return goog.object.clone(this.oPlayersById);
+_.GameImpl.prototype.playersById = function(){
+  return goog.object.clone(this.state.playersById);
 };
 
-__.Game.prototype.stacks = function(){ return this.aStacks.slice(); };
+_.GameImpl.prototype.stacks = function(){ return this.state.stacks.slice(); };
 
-__.Game.prototype.stacksByHoldingPlayerId = function(){
-  return goog.object.clone(this.oStacksHeldByPlayers);
+_.GameImpl.prototype.stacksByHoldingPlayerId = function(){
+  return goog.object.clone(this.state.stacksInPlay);
 };
 
 /** @return {?_.Player} */
-__.Game.prototype.myPlayer = function(){ return this.playerMe; };
+_.GameImpl.prototype.myPlayer = function(){ return this.state.playerMe; };
 
-__.Game.prototype.finishFetchState = function(jsonResult) {
+_.GameImpl.prototype.finishFetchState = function(jsonResult) {
   switch(jsonResult.getState()) {
     case Result.State.PENDING:
       throw "invalid state for call to finishFetchState";
     case Result.State.ERROR:
       throw jsonResult.getError();
     case Result.State.SUCCESS:
-      this.state.setFromJson(jsonResult.getValue());
+      return this.state.setFromJSON(jsonResult.getValue());
   }
 };
 
-///** @return {Array.<_.Stack>} */
-//_.Player.prototype.stacksHeld = function(){};
-//
-///** @return {string} */
-//_.Player.prototype.id = function(){};
-//
-///** @return {string} */
-//_.Player.prototype.name = function(){};
-//
-///** @return {string} */
-//_.Player.prototype.styleClass = function(){};
-//
-///** @return {Result} */
-//_.Stack.prototype.fetchState = function(){};
-//
-///** @return {?Array.<_.Drawing>} */
-//_.Stack.prototype.drawings = function(){};
-//
-///** @return {string} */
-//_.Stack.prototype.id = function(){};
-//
-///** @return {Result} */
-//_.Drawing.prototype.fetchState = function(){};
-//
-///** @return {?Array.<DrawPart>} */
-//_.Drawing.prototype.content = function(){};
-//
-///** @return {string} */
-//_.Drawing.prototype.id = function(){};
-//
-///** @return {_.Player} */
-//_.Drawing.prototype.player = function(){};
-//
+/**
+ * @constructor
+ */ 
+_.GameImpl.GameState = function() {
+  this.id = "";
+  this.isComplete = false;
+  this.isStarted = false;
+  this.lastTime = 0;
+  /**@type {Array.<_.Player>}*/
+  this.players = [];
+
+  /**@type {Object.<string, _.Player>}*/
+  this.playersById = {};
+
+  /**@type {Array.<_.Stack>}*/
+  this.stacks = [];
+
+  /**@type {Object.<string, _.Stack>}*/
+  this.stacksInPlay = {};
+  this.url = "";
+
+  /**@type {?_.Player}*/
+  this.playerMe = null;
+};
+
+_.GameImpl.GameState.prototype.setFromJSON = function(obj) {}
+
+/** @constructor */
+_.GameImpl.Player = function() {}
+/** @return {Array.<_.Stack>} */
+_.GameImpl.Player.prototype.stacksHeld = function(){};
+/** @return {string} */
+_.GameImpl.Player.prototype.id = function(){};
+/** @return {string} */
+_.GameImpl.Player.prototype.name = function(){};
+/** @return {string} */
+_.GameImpl.Player.prototype.styleClass = function(){};
+
+
+/** @constructor */
+_.GameImpl.Stack = function() {}
+/** @return {Result} */
+_.GameImpl.Stack.prototype.fetchState = function(){};
+/** @return {?Array.<_.Drawing>} */
+_.GameImpl.Stack.prototype.drawings = function(){};
+/** @return {string} */
+_.GameImpl.Stack.prototype.id = function(){};
+/** @return {Result} */
+_.GameImpl.Drawing.prototype.fetchState = function(){};
+/** @return {?Array.<DrawPart>} */
+_.GameImpl.Drawing.prototype.content = function(){};
+/** @return {string} */
+_.GameImpl.Drawing.prototype.id = function(){};
+/** @return {_.Player} */
+_.GameImpl.Drawing.prototype.player = function(){};
 });
